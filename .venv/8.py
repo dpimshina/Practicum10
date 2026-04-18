3c980e86e668b6172416066e05be031ff8895e67
dpimshina <d.pimshina@g.nsu.ru>
2026-04-18 13:03:51 +0700

    Update 8.py

--- .venv/8.py
+++ .venv/8.py
@@ -1,7 +1,7 @@
 def format_date(date_str):
+    """Преобразует дату из формата MM/DD/YYYY HH:MM:SS в DD.MM.YY HH:SS AM/PM."""
     try:
         date, time = date_str.split()
-
         month, day, year = date.split("/")
         hour, minute, second = time.split(":")
 
@@ -9,11 +9,11 @@ def format_date(date_str):
         day = int(day)
         hour = int(hour)
 
-        if month < 1 or month > 12:
+        if not (1 <= month <= 12):
             print("Ошибка")
             return
 
-        if day < 1 or day > 31:
+        if not (1 <= day <= 31):
             print("Ошибка")
             return
 
@@ -25,5 +25,5 @@ def format_date(date_str):
 
         print(f"{day:02}.{month:02}.{year[2:]} {hour:02}:{minute}:{second} {period}")
 
-    except:
+    except (ValueError:
         print("Ошибка формата")
